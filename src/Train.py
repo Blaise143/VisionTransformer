ef233c44a34944b6a11f8f1a0a4120836218bdf6
Blaise143 <blaiseappo@gmail.com>
2023-11-21 18:04:05 -0800

    Cleaned and refactored code

--- src/Train.py
+++ src/Train.py
@@ -3,40 +3,27 @@ import torch.nn as nn
 from VisionTransformer import VisionTransformer
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, random_split
-import matplotlib.pyplot as plt
 from tqdm.notebook import tqdm
 import numpy as np
 import wandb
+from HelperFunctions import calculate_accuracy
 
 lr = 3e-4
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-EPOCHS = 20
+EPOCHS = 1
 BATCH_SIZE = 64
 
 
-def calculate_accuracy(y_true, y_pred) -> float:
-    """
-    Calculates the accuracy of the model
-    :param y_true: True labels
-    :param y_pred: Predicted labels
-    :return: Accuracy
-    """
-    correct = torch.eq(y_true, y_pred).sum().item()
-    accuracy = (correct / len(y_pred)) * 100
-    return accuracy
-
-
-def train_2(ml_model, learning_rate=lr, image_size=64):
+def train(ml_model, learning_rate=lr, image_size=64):
     """
     Train a Vision Transformer model on a custom dataset.
-
     Args:
         ml_model (VisionTransformer): The Vision Transformer model to train.
         learning_rate (float): Learning rate for training.
         image_size (int): Size to which images are resized.
     """
 
+    # Logging to wandb to track the training
     wandb.init(project="ViT")
     wandb.config = {
         "learning_rate": learning_rate,
@@ -47,19 +34,22 @@ def train_2(ml_model, learning_rate=lr, image_size=64):
         # transforms.Resize((image_size, image_size)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x / 255.0),
-        transforms.Normalize((0.,), (1.,))
+        # transforms.Lambda(lambda x: x / 255.0),
+        # transforms.Normalize((0.,), (1.,))
     ])
 
     val_transform = transforms.Compose([
         # transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x / 255.0),
-        transforms.Normalize((0.,), (1.,))
+        # transforms.Lambda(lambda x: x / 255.0),
+        # transforms.Normalize((0.,), (1.,))
     ])
     # ---
     train_dataset = datasets.MNIST(root='./mnist_data', train=True, download=True, transform=train_transform)
     validation_dataset = datasets.MNIST(root='./mnist_data', train=False, download=True, transform=val_transform)
+
+    image_to_delete, label_to_delete = train_dataset[0]
+    image_2, label_2 = train_dataset[10]
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     validation_loader = DataLoader(dataset=validation_dataset, batch_size=BATCH_SIZE, shuffle=False)
@@ -85,13 +75,13 @@ def train_2(ml_model, learning_rate=lr, image_size=64):
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
             # print(images.shape)
-            print("yo")
+            # print("yo")
 
             # Forward pass
             outputs = ml_model(images)
             # _, outputs = torch.max(outputs, dim=1)
             # print(outputs)
-            print(outputs.shape, labels.shape)
+            # print(outputs.shape, labels.shape)
             loss = criterion(outputs, labels)
 
             # Backward pass and optimization
@@ -127,7 +117,6 @@ def train_2(ml_model, learning_rate=lr, image_size=64):
 
                 _, val_predicted = torch.max(outputs, dim=1)
                 correct_val += torch.eq(val_labels, val_predicted).sum().item()
-                print(f"correct_val: {correct_val}")
                 total_val += val_labels.size(0)
                 if epoch in [10, 20, 30]:
                     print(f"total_val: {total_val}")
@@ -144,36 +133,3 @@ def train_2(ml_model, learning_rate=lr, image_size=64):
         wandb.finish()
 
     return (train_losses, validation_losses), (train_accuracies, validation_accuracies)
-
-
-if __name__ == "__main__":
-    model = VisionTransformer(
-        image_size=28,
-        patch_size=4,
-        embed_dim=8,
-        num_layers=8,
-        num_heads=4,
-        num_classes=10,
-        in_channels=1,
-    )
-    # print(model)
-    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(n_params, "n params")
-    losses, accuracies = train_2(ml_model=model)
-    t_loss, v_loss = losses
-    t_acc, v_acc = accuracies
-    # print(t_loss, v_loss)
-    plt.plot(t_loss, label="Train loss")
-    plt.plot(v_loss, label="Validation loss")
-    plt.title("Losses over time")
-    plt.grid()
-    plt.legend()
-    plt.show()
-    plt.close()
-
-    plt.plot(t_acc, label=" Train accuracy")
-    plt.plot(v_acc, label="Validation Accuracy")
-    plt.title("Accuracies over time")
-    plt.legend()
-    plt.grid()
-    plt.show()
